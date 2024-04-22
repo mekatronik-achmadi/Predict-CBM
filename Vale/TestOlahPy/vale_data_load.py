1ac2e1f5b1105559767ac7184416869da9a94a48
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2024-04-22 13:30:45 +0700

    Olah Data Python

--- Vale/TestOlahPy/vale_data_load.py
+++ Vale/TestOlahPy/vale_data_load.py
@@ -1,4 +1,31 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import pandas
+from pprint import pprint
+
+class DataLoad():
+
+    """ Loading and Parse Data Source"""
+
+    def __init__(self):
+        # do nothing for now
+        pass
+
+    def from_xlxs(self,path,sheet=0):
+        pd_xlxs = pandas.read_excel(path,sheet,engine='openpyxl')
+        return pd_xlxs
+
+    def show_data(self,var_in):
+        if not isinstance(var_in, pandas.DataFrame):
+            print('Data input is not Panda DataFrame')
+            return
+
+        print('Data Summary:')
+        print(var_in)
+
+        print('Data Frame Header:')
+        pprint(var_in.columns.tolist())
+
+    def show_data_header(self,var_in,header_str):
+        pass
 
---
+++ Vale/TestOlahPy/vale_main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from vale_data_load import DataLoad
+
+datlod = DataLoad()
+data_lgs1 = datlod.from_xlxs('../OlahData/LGS_Overall_2017_2023_hourly.xlsx')
+datlod.show_data(data_lgs1)
+
