ac602b47d04a690edab850ec21cae78c555eacde
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2024-05-06 16:04:52 +0800

    Direct Request Plot

--- Vale/PredictPIServer/main_test.py
+++ Vale/PredictPIServer/main_test.py
@@ -1,18 +1,51 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+from threading import Thread as thd
+from time import sleep
+
 from ValePredictPI.vale_connect import ValeConnect
 
-server_root = '142.40.33.208'
-server_base = 'pti-pi'
-x_tag = 'U-LGS1-GB-X-PK-PK-70-AI'
+class ValeMain():
+    def __init__(self):
+        self.server_root = '142.40.33.208'
 
-conn = ValeConnect(server_root,server_base)
+        self.server_base = 'pti-pi'
+        self.x_tag = 'U-LGS1-GB-X-PK-PK-70-AI'
 
-x_tag_wid = conn.get_webid_point(x_tag)
-value_array = conn.get_stream_rec_json_value(x_tag_wid)
+        self.conn = ValeConnect(self.server_root,self.server_base)
 
-print(value_array)
-print(f"Retrieved Array {value_array.shape}")
-print("Tag Web ID: %s" % (x_tag_wid))
+        self.x_tag_wid = self.conn.get_webid_point(self.x_tag)
+        self.value_array = self.conn.get_stream_rec_json_value(self.x_tag_wid)
 
+        print(self.value_array)
+        print(f"Retrieved Array {self.value_array.shape}")
+        print("Tag Web ID: %s" % (self.x_tag_wid))
+
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111)
+        self.x_axis = np.arange(0,len(self.value_array),1)
+        self.line, = self.ax.plot(self.x_axis,self.value_array)
+
+        ani = animation.FuncAnimation(fig=self.fig, func=self.granim, interval=0.005,repeat=False)
+        ani._start()
+
+        thd(target=self.update_data).start()
+        plt.show()
+
+    def granim(self,args):
+        self.line.set_data(self.x_axis,self.value_array)
+
+    def update_data(self):
+        while True:
+            print('Request New Data')
+            self.value_array = self.conn.get_stream_rec_json_value(self.x_tag_wid)
+            print(self.value_array)
+            sleep(1)
+
+if __name__ == "__main__":
+    vale = ValeMain()
