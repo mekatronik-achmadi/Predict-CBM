a258c8beac8c90e00164a1801cb7b4b5f61001c9
MA Asyraf <maasyraf.edu@gmail.com>
2024-05-31 14:34:17 +0700

    update on Docs

--- Vale/Docs/ExistingPITag/export_to_json.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-pi_tag_list = pd.read_excel('PI_Tag_Resume.xlsx', sheet_name="pi_tag_list_update")
-json_str = pi_tag_list.to_json(orient='records')
-
-# Using a JSON string
-with open('json_data.json', 'w') as outfile:
-    outfile.write(json_str)
